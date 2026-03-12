6062e93959e684058c4820790a198958c55c3f7d
umux.com <63830487+X-Tips@users.noreply.github.com>
2026-03-12 17:38:17 +0800

    Add files via upload

---
+++ v6e/072r/mcuconf.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef STM32_PWM_USE_TIM3
+#define STM32_PWM_USE_TIM3 TRUE
+
+#undef STM32_SERIAL_USE_USART1
+#define STM32_SERIAL_USE_USART1 TRUE
+
+#undef STM32_USART1_USE_ALTERNATE_PINS
+#define STM32_USART1_USE_ALTERNATE_PINS TRUE
+
+#undef STM32_SPI_USE_SPI2
+#define STM32_SPI_USE_SPI2 FALSE
+
+#undef STM32_I2C_USE_I2C1
+#define STM32_I2C_USE_I2C1 FALSE
