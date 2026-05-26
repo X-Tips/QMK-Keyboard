376de59838a72d5ac35ba605d29d9f24b8554277
X-Tips <198892@gmail.com>
2026-05-26 11:48:16 +0800

    Update 2026-05-26 11:48

--- x26s/072c/config.h
+++ x26s/072c/config.h
@@ -1,9 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
 
-#define USB_VBUS_PIN A14
-#define WS2812_DI_PIN A6
-#define SPLIT_HAND_PIN A13
+#define USB_VBUS_PIN A10
+#define WS2812_DI_PIN A14
+#define SPLIT_HAND_PIN A6
 #define SERIAL_USART_TX_PIN B6
 #define SERIAL_USART_RX_PIN B7
 
